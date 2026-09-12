472f74229b7d0af708bdae83b51f32390ba9b96e
Rishabh751 <rishabhrajofficial02@gmail.com>
2026-09-13 00:16:16 +0530

    Final weather app with serverless backend and Caching header and vercel local region change for lower latency

--- api/weather.js
+++ api/weather.js
@@ -5,6 +5,8 @@ export default async function handler(request, response) {
     
     const fetchResponse = await fetch(url);
     const data = await fetchResponse.json();
+
+    response.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=86400');
     
     return response.status(fetchResponse.status).json(data);
 }
